1c356346ae0f0927f21a6f26c61482dc8c197d23
Andrian <andrian@toroki.com>
2012-07-12 10:54:32 +0300

    UI primitive interface.

---
+++ udf/src/udfAccountInfo.cpp
@@ -0,0 +1,18 @@
+#include "udfAccountInfo.h"
+
+udfAccountInfo::udfAccountInfo( wxWindow* parent )
+:
+AccountInfo( parent )
+{
+
+}
+
+void udfAccountInfo::OnSave( wxCommandEvent& event )
+{
+	// TODO: Implement OnSave
+}
+
+void udfAccountInfo::OnDiscard( wxCommandEvent& event )
+{
+	// TODO: Implement OnDiscard
+}
