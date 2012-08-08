4fd3213506a7a3ffc97cba4a37466d19b1f7ea23
andrian.yablonsky <andrian.yablonsky@gmail.com>
2012-08-09 00:55:34 +0300

    Add DBMgr api + datatypes.

--- dbmgr/src/dbmgr.cpp
+++ dbmgr/src/dbmgr.cpp
@@ -2,139 +2,6 @@
 #include "dbmgr.h"
 /* MySQL Connector/C++ specific headers */
 
-#include <cppconn/metadata.h>
-#include <cppconn/resultset_metadata.h>
-	
-#define DBHOST "tcp://192.169.11.182:3306"
-#define USER "andrian"
-#define PASSWORD "dataNet"
-#define DATABASE "udf"
-
-#define NUMOFFSET 100
-#define COLNAME 200
-
-
-static void retrieve_data_and_print (ResultSet *rs, int type, int colidx, string colname) {
-
-	/* retrieve the row count in the result set */
-	cout << "\nRetrieved " << rs -> rowsCount() << " row(s)." << endl;
-
-	cout << "\nCityName" << endl;
-	cout << "--------" << endl;
-
-	/* fetch the data : retrieve all the rows in the result set */
-	while (rs->next()) {
-		if (type == NUMOFFSET) {
-                       cout << rs -> getString(colidx) << endl;
-		} else if (type == COLNAME) {
-                       cout << rs -> getString(colname) << endl;
-		} // if-else
-	} // while
-
-	cout << endl;
-
-} // retrieve_data_and_print()
-
-static void retrieve_dbmetadata_and_print (Connection *dbcon) {
-
-	if (dbcon -> isClosed()) {
-		throw runtime_error("DatabaseMetaData FAILURE - database connection closed");
-	}
-
-	cout << "\nDatabase Metadata" << endl;
-	cout << "-----------------" << endl;
-
-	cout << boolalpha;
-
-	/* The following commented statement won't work with Connector/C++ 1.0.5 and later */
-	//auto_ptr < DatabaseMetaData > dbcon_meta (dbcon -> getMetaData());
-
-	DatabaseMetaData *dbcon_meta = dbcon -> getMetaData();
-
-	cout << "Database Product Name: " << dbcon_meta -> getDatabaseProductName() << endl;
-	cout << "Database Product Version: " << dbcon_meta -> getDatabaseProductVersion() << endl;
-	cout << "Database User Name: " << dbcon_meta -> getUserName() << endl << endl;
-
-	cout << "Driver name: " << dbcon_meta -> getDriverName() << endl;
-	cout << "Driver version: " << dbcon_meta -> getDriverVersion() << endl << endl;
-
-	cout << "Database in Read-Only Mode?: " << dbcon_meta -> isReadOnly() << endl;
-	cout << "Supports Transactions?: " << dbcon_meta -> supportsTransactions() << endl;
-	cout << "Supports DML Transactions only?: " << dbcon_meta -> supportsDataManipulationTransactionsOnly() << endl;
-	cout << "Supports Batch Updates?: " << dbcon_meta -> supportsBatchUpdates() << endl;
-	cout << "Supports Outer Joins?: " << dbcon_meta -> supportsOuterJoins() << endl;
-	cout << "Supports Multiple Transactions?: " << dbcon_meta -> supportsMultipleTransactions() << endl;
-	cout << "Supports Named Parameters?: " << dbcon_meta -> supportsNamedParameters() << endl;
-	cout << "Supports Statement Pooling?: " << dbcon_meta -> supportsStatementPooling() << endl;
-	cout << "Supports Stored Procedures?: " << dbcon_meta -> supportsStoredProcedures() << endl;
-	cout << "Supports Union?: " << dbcon_meta -> supportsUnion() << endl << endl;
-
-	cout << "Maximum Connections: " << dbcon_meta -> getMaxConnections() << endl;
-	cout << "Maximum Columns per Table: " << dbcon_meta -> getMaxColumnsInTable() << endl;
-	cout << "Maximum Columns per Index: " << dbcon_meta -> getMaxColumnsInIndex() << endl;
-	cout << "Maximum Row Size per Table: " << dbcon_meta -> getMaxRowSize() << " bytes" << endl;
-
-	cout << "\nDatabase schemas: " << endl;
-
-	auto_ptr < ResultSet > rs ( dbcon_meta -> getSchemas());
-
-	cout << "\nTotal number of schemas = " << rs -> rowsCount() << endl;
-	cout << endl;
-
-	int row = 1;
-
-	while (rs -> next()) {
-		cout << "\t" << row << ". " << rs -> getString("TABLE_SCHEM") << endl;
-		++row;
-	} // while
-
-	cout << endl << endl;
-
-} // retrieve_dbmetadata_and_print()
-
-static void retrieve_rsmetadata_and_print (ResultSet *rs) {
-
-	if (rs -> rowsCount() == 0) {
-		throw runtime_error("ResultSetMetaData FAILURE - no records in the result set");
-	}
-
-	cout << "ResultSet Metadata" << endl;
-	cout << "------------------" << endl;
-
-	/* The following commented statement won't work with Connector/C++ 1.0.5 and later */
-	//auto_ptr < ResultSetMetaData > res_meta ( rs -> getMetaData() );
-
-	ResultSetMetaData *res_meta = rs -> getMetaData();
-	
-	int numcols = res_meta -> getColumnCount();
-	cout << "\nNumber of columns in the result set = " << numcols << endl << endl;
-
-	cout.width(20);
-	cout << "Column Name/Label";
-	cout.width(20);
-	cout << "Column Type";
-	cout.width(20);
-	cout << "Column Size" << endl;
-	
-	rs->next();
-		
-	for (int i = 0; i < numcols; ++i) {
-		cout.width(20);
-		cout << res_meta -> getColumnLabel (i+1);
-		cout.width(20); 
-		cout << res_meta -> getColumnTypeName (i+1);
-		cout.width(20); 
-		cout << res_meta -> getColumnDisplaySize (i+1) << endl;
-		cout.width(20); 
-		cout << rs -> getString(i+1) << endl;
-	}
-
-	cout << "\nColumn \"" << res_meta -> getColumnLabel(1);
-	cout << "\" belongs to the Table: \"" << res_meta -> getTableName(1);
-	cout << "\" which belongs to the Schema: \"" << res_meta -> getSchemaName(1) << "\"" << endl << endl;
-
-} // retrieve_rsmetadata_and_print()
-
 
 int dbmgr_test(void)
 {
@@ -147,31 +14,31 @@ int dbmgr_test(void)
 
 	int updatecount = 0;
 
-	/* initiate url, user, password and database variables */
-	string url(DBHOST);
-	const string user(USER);
-	const string password(PASSWORD);
-	const string database(DATABASE);
+	// initiate url, user, password and database variables */
+	//string url(DBHOST);
+	//const string user(USER);
+	//const string password(PASSWORD);
+	//const string database(DATABASE);
 
 	try {
 		driver = get_driver_instance();
-		
-		/* create a database connection using the Driver */
+		/*
+		// create a database connection using the Driver 
 		con = driver -> connect(url, user, password);
 
-		/* alternate syntax using auto_ptr to create the db connection */
+		// alternate syntax using auto_ptr to create the db connection 
 		//auto_ptr  con (driver -> connect(url, user, password));
 
-		/* turn off the autocommit */
+		// turn off the autocommit 
 		con -> setAutoCommit(0);
 		
-		/* select appropriate database schema */
+		// select appropriate database schema 
 		con -> setSchema(database);
 		
-		/* retrieve and display the database metadata */
+		// retrieve and display the database metadata 
 		//retrieve_dbmetadata_and_print (con);
 
-		/* create a statement object */
+		// create a statement object 
 		stmt = con -> createStatement();
 
 		cout << "Executing the Query: \"SELECT * FROM cities\" .." << endl;
@@ -179,7 +46,7 @@ int dbmgr_test(void)
 		//res = stmt -> executeQuery ("set character_set_client='utf8'");
 		//res = stmt -> executeQuery ("set character_set_results='utf8'");
 		//res = stmt -> executeQuery ("set collation_connection='utf8_general_ci'");
-		/* run a query which returns exactly one result set */
+		// run a query which returns exactly one result set 
 		res = stmt->executeQuery("SELECT * FROM age_category");
 
 		cout << "Retrieving the result set ..." << endl;
@@ -189,15 +56,15 @@ int dbmgr_test(void)
 		}
 		//retrieve_rsmetadata_and_print(res);
 
-		/* retrieve the data from the result set and display on stdout */
+		// retrieve the data from the result set and display on stdout 
 		//retrieve_data_and_print (res, NUMOFFSET, 1, string("CityName"));
 
-		/* retrieve and display the result set metadata */
+		// retrieve and display the result set metadata 
 		//retrieve_rsmetadata_and_print (res);
 
 		//cout << "Demonstrating Prepared Statements .. " << endl << endl;
 
-		/* insert couple of rows of data into City table using Prepared Statements */
+		// insert couple of rows of data into City table using Prepared Statements 
 		//prep_stmt = con -> prepareStatement ("INSERTmysql_query ("set character_set_client='cp1251'");
 
 		//cout << "\tInserting \"London, UK\" into the table, City .." << endl;
@@ -222,22 +89,22 @@ int dbmgr_test(void)
 
 		//cout << "\nQuerying the City table again .." << endl;
 
-		/* re-use result set object */
+		// re-use result set object 
 		res = NULL;
 		//res = stmt -> executeQuery ("SELECT * FROM City");
 
-		/* retrieve the data from the result set and display on stdout */
+		// retrieve the data from the result set and display on stdout 
 		//retrieve_data_and_print (res, COLNAME, 1, string ("CityName"));
 
 		cout << "Cleaning up the resources ... exit!" << endl;
 
-		/* Clean up */
+		// Clean up 
 		delete res;
 		delete stmt;
 		delete prep_stmt;
 		con -> close();
 		delete con;
-
+		//*/
 	} catch (SQLException &e) {
 		cout << "ERROR: SQLException in " << __FILE__;
 		cout << " (" << __func__<< ") on line " << __LINE__ << endl;
@@ -266,4 +133,364 @@ int dbmgr_test(void)
 
 	return EXIT_SUCCESS;
 
+}
+
+tUDF_AgeCategory* GetAgeCategoryList(tUDF_AgeCategory* filter)
+{
+}
+
+tUDF_AgeCategory* FindAgeCategory(tUDF_AgeCategory* filter)
+{
+}
+
+long AddAgeCategory(tUDF_AgeCategory* data)
+{
+}
+
+long RemoveAgeCategory(tUDF_AgeCategory* item)
+{
+}
+
+tUDF_Category* GetCategoryList(tUDF_Category* filter)
+{
+}
+
+tUDF_Category* FindCategory(tUDF_Category* filter)
+{
+}
+
+long AddCategory(tUDF_Category* data)
+{
+}
+
+long RemoveCategory(tUDF_Category* item)
+{
+}
+
+tUDF_Championship* GetChampionshipList(tUDF_Championship* filter)
+{
+}
+
+tUDF_Championship* FindChampionship(tUDF_Championship* filter)
+{
+}
+
+long AddChampionship(tUDF_Championship* data)
+{
+}
+
+long RemoveChampionship(tUDF_Championship* item)
+{
+}
+
+tUDF_ChampionshipCategories* GetChampionshipCategoriesList(tUDF_ChampionshipCategories* filter)
+{
+}
+
+tUDF_ChampionshipCategories* FindChampionshipCategories(tUDF_ChampionshipCategories* filter)
+{
+}
+
+long AddChampionshipCategories(tUDF_ChampionshipCategories* data)
+{
+}
+
+long RemoveChampionshipCategories(tUDF_ChampionshipCategories* item)
+{
+}
+
+tUDF_ChampionshipJudgesMark* GetChampionshipJudgesMarkList(tUDF_ChampionshipJudgesMark* filter)
+{
+}
+
+tUDF_ChampionshipJudgesMark* FindChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* filter)
+{
+}
+
+long AddChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* data)
+{
+}
+
+long RemoveChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* item)
+{
+}
+
+tUDF_ChampionshipJudgesTeam* GetChampionshipJudgesTeamList(tUDF_ChampionshipJudgesTeam* filter)
+{
+}
+
+tUDF_ChampionshipJudgesTeam* FindChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* filter)
+{
+}
+
+long AddChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* data)
+{
+}
+
+long RemoveChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* item)
+{
+}
+
+tUDF_ChampionshipTeam* GetChampionshipTeamList(tUDF_ChampionshipTeam* filter)
+{
+}
+
+tUDF_ChampionshipTeam* FindChampionshipTeam(tUDF_ChampionshipTeam* filter)
+{
+}
+
+long AddChampionshipTeam(tUDF_ChampionshipTeam* data)
+{
+}
+
+long RemoveChampionshipTeam(tUDF_ChampionshipTeam* item)
+{
+}
+
+tUDF_ChampionshipType* GetChampionshipTypeList(tUDF_ChampionshipType* filter)
+{
+}
+
+long AddChampionshipType(tUDF_ChampionshipType* data)
+{
+}
+
+long RemoveChampionshipType(tUDF_ChampionshipType* item)
+{
+}
+
+tUDF_ChampionshipTeamCategories* GetChampionshipTeamCategoriesList(tUDF_ChampionshipTeamCategories* filter)
+{
+}
+
+tUDF_ChampionshipTeamCategories* FindChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* filter)
+{
+}
+
+long AddChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* data)
+{
+}
+
+long RemoveChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* item)
+{
+}
+
+tUDF_Cities* GetCitiesList(tUDF_Cities* filter)
+{
+}
+
+tUDF_Cities* FindCities(tUDF_Cities* filter)
+{
+}
+
+long AddCities(tUDF_Cities* data)
+{
+}
+
+long RemoveCities(tUDF_Cities* item)
+{
+}
+
+tUDF_Clubs* GetClubsList(tUDF_Clubs* filter)
+{
+}
+
+tUDF_Clubs* FindClubs(tUDF_Clubs* filter)
+{
+}
+
+long AddClubs(tUDF_Clubs* data)
+{
+}
+
+long RemoveClubs(tUDF_Clubs* item)
+{
+}
+
+tUDF_Counties* GetCountiesList(tUDF_Counties* filter)
+{
+}
+
+tUDF_Counties* FindCounties(tUDF_Counties* filter)
+{
+}
+
+long AddCounties(tUDF_Counties* data)
+{
+}
+
+long RemoveCounties(tUDF_Counties* item)
+{
+}
+
+tUDF_DanceTypes* GetDanceTypesyList(tUDF_DanceTypes* filter)
+{
+}
+
+tUDF_DanceTypes* FindDanceTypes(tUDF_DanceTypes* filter)
+{
+}
+
+long AddDanceTypes(tUDF_DanceTypes* data)
+{
+}
+
+long RemoveDanceTypes(tUDF_DanceTypes* item)
+{
+}
+
+tUDF_Dancers* GetDancersList(tUDF_Dancers* filter)
+{
+}
+
+tUDF_Dancers* FindDancers(tUDF_Dancers* filter)
+{
+}
+
+long AddDancers(tUDF_Dancers* data)
+{
+}
+
+long RemoveDancers(tUDF_Dancers* item)
+{
+}
+
+tUDF_Gender* GetGenderList(tUDF_Gender* filter)
+{
+}
+
+tUDF_Gender* FindGender(tUDF_Gender* filter)
+{
+}
+
+long AddGender(tUDF_Gender* data)
+{
+}
+
+long RemoveGender(tUDF_Gender* item)
+{
+}
+
+tUDF_Judges* GetJudgesList(tUDF_Judges* filter)
+{
+}
+
+tUDF_Judges* FindJudges(tUDF_Judges* filter)
+{
+}
+
+long AddJudges(tUDF_Judges* data)
+{
+}
+
+long RemoveJudges(tUDF_Judges* item)
+{
+}
+
+tUDF_JudgesCategoriesHave* GetJudgesCategoriesHaveList(tUDF_JudgesCategoriesHave* filter)
+{
+}
+
+tUDF_JudgesCategoriesHave* FindJudgesCategoriesHave(tUDF_JudgesCategoriesHave* filter)
+{
+}
+
+long AddJudgesCategoriesHave(tUDF_JudgesCategoriesHave* data)
+{
+}
+
+long RemoveJudgesCategoriesHave(tUDF_JudgesCategoriesHave* item)
+{
+}
+
+tUDF_JudgesCategoriesName* GetJudgesCategoriesNameList(tUDF_JudgesCategoriesName* filter)
+{
+}
+
+tUDF_JudgesCategoriesName* FindJudgesCategoriesName(tUDF_JudgesCategoriesName* filter)
+{
+}
+
+long AddJudgesCategoriesName(tUDF_JudgesCategoriesName* data)
+{
+}
+
+long RemoveJudgesCategoriesName(tUDF_JudgesCategoriesName* item)
+{
+}
+
+tUDF_Liga* GetLigaList(tUDF_Liga* filter)
+{
+}
+
+tUDF_Liga* FindLiga(tUDF_Liga* filter)
+{
+}
+
+long AddLiga(tUDF_Liga* data)
+{
+}
+
+long RemoveLiga(tUDF_Liga* item)
+{
+}
+
+tUDF_Treners* GetTrenersList(tUDF_Treners* filter)
+{
+}
+
+tUDF_Treners* FindTreners(tUDF_Treners* filter)
+{
+}
+
+long AddTreners(tUDF_Treners* data)
+{
+}
+
+long RemoveTreners(tUDF_Treners* item)
+{
+}
+
+void ConnectionOpen(string host, int port, string user, string pass)
+{
+}
+
+void ConnectionClose()
+{
+}
+
+tUDF_LPCONNECTION GetConnection()
+{
+}
+
+tUDF_LPRESULTS ExecureQuery(string query)
+{
+}
+
+tUDF_LPSTATEMENT PrepareStatement(string statement)
+{
+}
+
+void PreparedStatementSetString(tUDF_LPSTATEMENT statement, string str)
+{
+}
+
+int PreparedStatementExecute(tUDF_LPSTATEMENT statement)
+{
+}
+
+tUDF_LPSAVEPOINT SetSavepoint(string name)
+{
+}
+
+void ReleaseSavepoint(tUDF_LPSAVEPOINT save)
+{
+}
+
+void Rollback(tUDF_LPSAVEPOINT save)
+{
+}
+
+void Commit()
+{
 }
--- dbmgr/src/dbmgr.h
+++ dbmgr/src/dbmgr.h
@@ -1,9 +1,5 @@
-#ifndef __DBMGR_H__
-#define __DBMGR_H__
-
-#ifdef __cplusplus
-extern "C" {
-#endif
+#ifndef __UDF_DBMGR_H__
+#define __UDF_DBMGR_H__
 
 #include <iostream>
 #include <sstream>
@@ -11,16 +7,22 @@ extern "C" {
 #include <string>
 #include <stdexcept>
 
-#include <cppconn/driver.h>
-#include <cppconn/connection.h>
-#include <cppconn/statement.h>
-#include <cppconn/prepared_statement.h>
-#include <cppconn/resultset.h>
-#include <cppconn/exception.h>
-#include <cppconn/warning.h>
+#include "cppconn/driver.h"
+#include "cppconn/connection.h"
+#include "cppconn/statement.h"
+#include "cppconn/prepared_statement.h"
+#include "cppconn/resultset.h"
+#include "cppconn/exception.h"
+#include "cppconn/warning.h"
+
+#include "map"
 
 using namespace std;
 using namespace sql;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef	Driver				tUDF_DRIVER;
 typedef	Driver*				tUDF_LPDRIVER;
@@ -40,29 +42,43 @@ typedef	PreparedStatement*	tUDF_LPPREPAREDSTATEMENT;
 typedef	Savepoint			tUDF_SAVEPOINT;
 typedef	Savepoint*			tUDF_LPSAVEPOINT;
 
-#include "string"
-#include "map"
+#define udfRegisterMap(keytype, valueType, tname)	\
+typedef map< keytype, valueType > tUDF_##tname; \
+typedef map< keytype, valueType >::iterator tUDF_##tname##Iterator;
 
-#define udfRegisterMap(keytype, valueType, name)	\
-typedef map<keytype, valueType> tUDF_Map##name; \
-typedef map<keytype, valueType>::iterator tUDF_MapIterator##name; 
-
-
+/*
+ * AgeCategory datatype and API
+ */
 typedef struct{
 	int				id;
 	string			descr;
 } tUDF_AGE_CATEGORY;
-typedef map<int, tUDF_AGE_CATEGORY> tUDF_AgeCategoryMap;
-typedef map<int, tUDF_AGE_CATEGORY>::iterator tUDF_AgeCategoryMapIterator;
+udfRegisterMap(int, tUDF_AGE_CATEGORY, AgeCategory);
 
+tUDF_AgeCategory*	GetAgeCategoryList(tUDF_AgeCategory* filter);
+tUDF_AgeCategory*	FindAgeCategory(tUDF_AgeCategory* filter);
+long				AddAgeCategory(tUDF_AgeCategory* data);
+long				RemoveAgeCategory(tUDF_AgeCategory* item);
+
+/*
+ * Category datatype and API
+ */
 typedef struct{
 	unsigned long 	id;
 	int				dance;
 	int				liga;
 	int				gender;
 } tUDF_CATEGORY;
-udfRegisterMap(unsigned long, tUDF_CATEGORY, tUDF_Category)
+udfRegisterMap(int, tUDF_CATEGORY, Category);
 
+tUDF_Category*		GetCategoryList(tUDF_Category* filter);
+tUDF_Category*		FindCategory(tUDF_Category* filter);
+long				AddCategory(tUDF_Category* data);
+long				RemoveCategory(tUDF_Category* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	int				type;
@@ -70,14 +86,30 @@ typedef struct{
 	string 			additional_info;
 	int				city;
 } tUDF_CHAMPIONSHIP;
-typedef tUDF_CHAMPIONSHIP* tUDF_LPCHAMPIONSHIP;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP, Championship);
 
+tUDF_Championship*	GetChampionshipList(tUDF_Championship* filter);
+tUDF_Championship*	FindChampionship(tUDF_Championship* filter);
+long				AddChampionship(tUDF_Championship* data);
+long				RemoveChampionship(tUDF_Championship* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	championship_id;
 	unsigned long	category_id;
 } tUDF_CHAMPIONSHIP_CATEGORIES;
-typedef tUDF_CHAMPIONSHIP_CATEGORIES* tUDF_LPCHAMPIONSHIP_CATEGORIES;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_CATEGORIES, ChampionshipCategories);
 
+tUDF_ChampionshipCategories*	GetChampionshipCategoriesList(tUDF_ChampionshipCategories* filter);
+tUDF_ChampionshipCategories*	FindChampionshipCategories(tUDF_ChampionshipCategories* filter);
+long							AddChampionshipCategories(tUDF_ChampionshipCategories* data);
+long							RemoveChampionshipCategories(tUDF_ChampionshipCategories* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	championship_id;
 	unsigned long	judge_id;
@@ -85,14 +117,30 @@ typedef struct{
 	unsigned long	category_id;
 	int				mark;
 } tUDF_CHAMPIONSHIP_JUDGES_MARK;
-typedef tUDF_CHAMPIONSHIP_JUDGES_MARK* tUDF_LPCHAMPIONSHIP_JUDGES_MARK;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_JUDGES_MARK, ChampionshipJudgesMark);
 
+tUDF_ChampionshipJudgesMark*	GetChampionshipJudgesMarkList(tUDF_ChampionshipJudgesMark* filter);
+tUDF_ChampionshipJudgesMark*	FindChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* filter);
+long							AddChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* data);
+long							RemoveChampionshipJudgesMark(tUDF_ChampionshipJudgesMark* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	championship_id;
 	unsigned long	judge_id;
 } tUDF_CHAMPIONSHIP_JUDGES_TEAM;
-typedef tUDF_CHAMPIONSHIP_JUDGES_TEAM* tUDF_LPCHAMPIONSHIP_JUDGES_TEAM;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_JUDGES_TEAM, ChampionshipJudgesTeam);
 
+tUDF_ChampionshipJudgesTeam*	GetChampionshipJudgesTeamList(tUDF_ChampionshipJudgesTeam* filter);
+tUDF_ChampionshipJudgesTeam*	FindChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* filter);
+long							AddChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* data);
+long							RemoveChampionshipJudgesTeam(tUDF_ChampionshipJudgesTeam* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	unsigned long	dancer_id;
@@ -100,27 +148,59 @@ typedef struct{
 	string			composition_name;
 	unsigned long	start_number;
 } tUDF_CHAMPIONSHIP_TEAM;
-typedef tUDF_CHAMPIONSHIP_TEAM* tUDF_LPCHAMPIONSHIP_TEAM;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_TEAM, ChampionshipTeam);
 
+tUDF_ChampionshipTeam*		GetChampionshipTeamList(tUDF_ChampionshipTeam* filter);
+tUDF_ChampionshipTeam*		FindChampionshipTeam(tUDF_ChampionshipTeam* filter);
+long						AddChampionshipTeam(tUDF_ChampionshipTeam* data);
+long						RemoveChampionshipTeam(tUDF_ChampionshipTeam* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned int	id;
 	string			name;
 } tUDF_CHAMPIONSHIP_TYPE;
-typedef tUDF_CHAMPIONSHIP_TYPE* tUDF_LPCHAMPIONSHIP_TYPE;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_TYPE, ChampionshipType);
 
+tUDF_ChampionshipType*		GetChampionshipTypeList(tUDF_ChampionshipType* filter);
+tUDF_ChampionshipType*		FindChampionshipType(tUDF_ChampionshipType* filter);
+long						AddChampionshipType(tUDF_ChampionshipType* data);
+long						RemoveChampionshipType(tUDF_ChampionshipType* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	team_id;
 	unsigned long	category_id;
 } tUDF_CHAMPIONSHIP_TEAM_CATEGORIES;
-typedef tUDF_CHAMPIONSHIP_TEAM_CATEGORIES* tUDF_LPCHAMPIONSHIP_TEAM_CATEGORIES;
+udfRegisterMap(int, tUDF_CHAMPIONSHIP_TEAM_CATEGORIES, ChampionshipTeamCategories);
 
+tUDF_ChampionshipTeamCategories*	GetChampionshipTeamCategoriesList(tUDF_ChampionshipTeamCategories* filter);
+tUDF_ChampionshipTeamCategories*	FindChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* filter);
+long								AddChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* data);
+long								RemoveChampionshipTeamCategories(tUDF_ChampionshipTeamCategories* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned int	id;
 	unsigned int	country_id;
 	string			name;
 } tUDF_CITIES;
-typedef tUDF_CITIES* tUDF_LPCITIES;
+udfRegisterMap(int, tUDF_CITIES, Cities);
 
+tUDF_Cities*		GetCitiesList(tUDF_Cities* filter);
+tUDF_Cities*		FindCities(tUDF_Cities* filter);
+long				AddCities(tUDF_Cities* data);
+long				RemoveCities(tUDF_Cities* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	string			name;
@@ -138,20 +218,44 @@ typedef struct{
 	string			director_phone;
 	string			director_email;
 } tUDF_CLUBS;
-typedef tUDF_CLUBS* tUDF_LPCLUBS;
+udfRegisterMap(int, tUDF_CLUBS, Clubs);
 
+tUDF_Clubs*			GetClubsList(tUDF_Clubs* filter);
+tUDF_Clubs*			FindClubs(tUDF_Clubs* filter);
+long				AddClubs(tUDF_Clubs* data);
+long				RemoveClubs(tUDF_Clubs* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned int	id;
 	string			name;
 } tUDF_COUNTRIES;
-typedef tUDF_COUNTRIES* tUDF_LPCOUNTRIES;
+udfRegisterMap(int, tUDF_COUNTRIES, Counties);
 
+tUDF_Counties*		GetCountiesList(tUDF_Counties* filter);
+tUDF_Counties*		FindCounties(tUDF_Counties* filter);
+long				AddCounties(tUDF_Counties* data);
+long				RemoveCounties(tUDF_Counties* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned int	id;
 	string			name;
 } tUDF_DANCE_TYPES;
-typedef tUDF_DANCE_TYPES* tUDF_LPDANCE_TYPES;
+udfRegisterMap(int, tUDF_DANCE_TYPES, DanceTypes);
 
+tUDF_DanceTypes*	GetDanceTypesyList(tUDF_DanceTypes* filter);
+tUDF_DanceTypes*	FindDanceTypes(tUDF_DanceTypes* filter);
+long				AddDanceTypes(tUDF_DanceTypes* data);
+long				RemoveDanceTypes(tUDF_DanceTypes* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	unsigned long	club_id;
@@ -167,14 +271,30 @@ typedef struct{
 	int64_t			expire_date;
 	int64_t			reg_date;
 } tUDF_DANCERS;
-typedef tUDF_DANCERS* tUDF_LPDANCERS;
+udfRegisterMap(int, tUDF_DANCERS, Dancers);
 
+tUDF_Dancers*		GetDancersList(tUDF_Dancers* filter);
+tUDF_Dancers*		FindDancers(tUDF_Dancers* filter);
+long				AddDancers(tUDF_Dancers* data);
+long				RemoveDancers(tUDF_Dancers* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	int				id;
 	string			name;
 } tUDF_GENDER;
-typedef tUDF_GENDER* tUDF_LPGENDER;
+udfRegisterMap(int, tUDF_GENDER, Gender);
 
+tUDF_Gender*		GetGenderList(tUDF_Gender* filter);
+tUDF_Gender*		FindGender(tUDF_Gender* filter);
+long				AddGender(tUDF_Gender* data);
+long				RemoveGender(tUDF_Gender* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	string			name;
@@ -185,26 +305,58 @@ typedef struct{
 	int64_t			pay_date;
 	int64_t			expire_date;
 } tUDF_JUDGES;
-typedef tUDF_JUDGES* tUDF_LPJUDGES;
+udfRegisterMap(int, tUDF_JUDGES, Judges);
 
+tUDF_Judges*		GetJudgesList(tUDF_Judges* filter);
+tUDF_Judges*		FindJudges(tUDF_Judges* filter);
+long				AddJudges(tUDF_Judges* data);
+long				RemoveJudges(tUDF_Judges* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	judge_id;
 	int				cat_id;
 } tUDF_JUDGES_CATEGORIES_HAVE;
-typedef tUDF_JUDGES_CATEGORIES_HAVE* tUDF_LPJUDGES_CATEGORIES_HAVE;
+udfRegisterMap(int, tUDF_JUDGES_CATEGORIES_HAVE, JudgesCategoriesHave);
 
+tUDF_JudgesCategoriesHave*		GetJudgesCategoriesHaveList(tUDF_JudgesCategoriesHave* filter);
+tUDF_JudgesCategoriesHave*		FindJudgesCategoriesHave(tUDF_JudgesCategoriesHave* filter);
+long							AddJudgesCategoriesHave(tUDF_JudgesCategoriesHave* data);
+long							RemoveJudgesCategoriesHave(tUDF_JudgesCategoriesHave* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	int				id;
 	string			name;
 } tUDF_JUDGES_CATEGORIES_NAME;
-typedef tUDF_JUDGES_CATEGORIES_NAME* tUDF_LPJUDGES_CATEGORIES_NAME;
+udfRegisterMap(int, tUDF_JUDGES_CATEGORIES_NAME, JudgesCategoriesName);
 
+tUDF_JudgesCategoriesName*		GetJudgesCategoriesNameList(tUDF_JudgesCategoriesName* filter);
+tUDF_JudgesCategoriesName*		FindJudgesCategoriesName(tUDF_JudgesCategoriesName* filter);
+long							AddJudgesCategoriesName(tUDF_JudgesCategoriesName* data);
+long							RemoveJudgesCategoriesName(tUDF_JudgesCategoriesName* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	int				id;
 	string			name;
 } tUDF_LIGA;
-typedef tUDF_LIGA*	tUDF_LPLIGA;
+udfRegisterMap(int, tUDF_LIGA, Liga);
 
+tUDF_Liga*			GetLigaList(tUDF_Liga* filter);
+tUDF_Liga*			FindLiga(tUDF_Liga* filter);
+long				AddLiga(tUDF_Liga* data);
+long				RemoveLiga(tUDF_Liga* item);
+
+/*
+ * Championship datatype and API
+ */
 typedef struct{
 	unsigned long	id;
 	unsigned long	club_id;
@@ -217,7 +369,12 @@ typedef struct{
 	int64_t			pay_date;
 	int64_t			expire_date;
 } tUDF_TRENERS;
-typedef tUDF_TRENERS* tUDF_LPTRENERS;
+udfRegisterMap(int, tUDF_TRENERS, Treners);
+
+tUDF_Treners*		GetTrenersList(tUDF_Treners* filter);
+tUDF_Treners*		FindTreners(tUDF_Treners* filter);
+long				AddTreners(tUDF_Treners* data);
+long				RemoveTreners(tUDF_Treners* item);
 
 /*******************************************************************************
  * DB Manager API
@@ -228,13 +385,14 @@ typedef tUDF_TRENERS* tUDF_LPTRENERS;
 void				ConnectionOpen(string host, int port, string user, string pass);
 void				ConnectionClose();
 tUDF_LPCONNECTION	GetConnection();
-
-// 
-// Age categories
-// 
-long	AgeCategoryGetFirst(tUDF_LPAGE_CATEGORY data);
-long	AgeCategoryGetNext(tUDF_LPAGE_CATEGORY data);
-long	AgeCategoryGetAll(tUDF_LPAGE_CATEGORY* data);
+tUDF_LPRESULTS		ExecureQuery(string query);
+tUDF_LPSTATEMENT	PrepareStatement(string statement);
+void				PreparedStatementSetString(tUDF_LPSTATEMENT statement, string str);
+int					PreparedStatementExecute(tUDF_LPSTATEMENT statement);
+tUDF_LPSAVEPOINT	SetSavepoint(string name);
+void				ReleaseSavepoint(tUDF_LPSAVEPOINT save);
+void				Rollback(tUDF_LPSAVEPOINT save);
+void				Commit();
 
 int dbmgr_test();
 
@@ -242,4 +400,4 @@ int dbmgr_test();
 }
 #endif
 
-#endif //__DBMGR_H__
+#endif //__UDF_DBMGR_H__
