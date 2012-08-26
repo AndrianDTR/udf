4b12e6b0962c35b1ae09db51634fb694c1d22d6c
Andrian <andrian@andrian-laptop.(none)>
2012-08-26 11:13:57 +0300

    Build LINUX ok.

--- common/src/connection_data.h
+++ common/src/connection_data.h
@@ -1,9 +1,9 @@
 #ifndef __UDFDATATYPES_H__
 #define __UDFDATATYPES_H__
 
-#define	szUrl		"192.169.10.17"
+#define	szUrl			"localhost"
 #define	szUser		"andrian"
 #define	szPass		"dataNet"
-#define	szSchema	"udf"
+#define	szSchema		"udf"
 
 #endif //__UDFDATATYPES_H__
