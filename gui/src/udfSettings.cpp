02bdae8e445c6e509e914f69878655baebd085f3
Andrian <andrian@toroki.com>
2012-11-05 15:21:24 +0200

    Settingd dialog has been implemented

--- gui/src/udfSettings.cpp
+++ gui/src/udfSettings.cpp
@@ -4,15 +4,73 @@
 #include "udfuiutils.h"
 #include "string_def.h"
 
+
+
 udfSettings::udfSettings( wxWindow* parent )
 : Settings( parent )
 , m_pCon(NULL)
+, m_pConf(NULL)
 {
+	m_pCon = CDbManager::Instance()->GetConnection();
+	m_pConf = new wxConfig("udf");
+
 	Refresh();
+}
+
+udfSettings::~udfSettings()
+{
+	if(m_pConf)
+	{
+		delete m_pConf;
+		m_pConf = NULL;
+	}
 }
 
 void udfSettings::OnSave( wxCommandEvent& event )
 {
+	m_pConf->Write("host", m_textServer->GetValue());
+	m_pConf->Write("db",  m_textDatabase->GetValue());
+	m_pConf->Write("user", m_textUser->GetValue());
+	m_pConf->Write("pass", m_textPass->GetValue());
+
+	CTourTypesTable::tDATA tourData = {0};
+	CTourTypesTable table(m_pCon);
+
+	table.GetRow(1, tourData);
+	m_textMaxFinal->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMinFinal->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(1, tourData);
+
+	table.GetRow(2, tourData);
+	m_textMax12->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin12->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(2, tourData);
+
+	table.GetRow(3, tourData);
+	m_textMax14->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin14->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(3, tourData);
+
+	table.GetRow(4, tourData);
+	m_textMax18->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin18->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(4, tourData);
+
+	table.GetRow(5, tourData);
+	m_textMax116->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin116->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(5, tourData);
+
+	table.GetRow(6, tourData);
+	m_textMax132->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin132->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(6, tourData);
+
+	table.GetRow(7, tourData);
+	m_textMax164->GetValue().ToULong((unsigned long*)&tourData.max);
+	m_textMin164->GetValue().ToULong((unsigned long*)&tourData.min);
+	table.UpdateRow(7, tourData);
+
 	EndModal(wxID_OK);
 }
 
@@ -23,9 +81,52 @@ void udfSettings::OnDiscard( wxCommandEvent& event )
 
 void udfSettings::Refresh()
 {
-	/*
-	 * 1. Read connection data
-	 * 2. Read tour type data
-	 */
+	do
+	{
+		/*
+		 * 1. Read connection data
+		 * 2. Read tour type data
+		 */
+		if(!m_pConf)
+			break;
+
+		m_textServer->SetValue(m_pConf->Read("host", "localhost:3306"));
+		m_textDatabase->SetValue(m_pConf->Read("db", "udf"));
+		m_textUser->SetValue(m_pConf->Read("user", "andrian"));
+		m_textPass->SetValue(m_pConf->Read("pass", "testpass"));
+
+		CTourTypesTable::tDATA tourData = {0};
+		CTourTypesTable table(m_pCon);
+
+		table.GetRow(1, tourData);
+		m_textMaxFinal->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMinFinal->SetValue(wxString::Format(_("%d"), tourData.min));
+		__info("Final: id=%d, min=%d, max=%d", tourData.id, tourData.min, tourData.max);
+
+		table.GetRow(2, tourData);
+		m_textMax12->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin12->SetValue(wxString::Format(_("%d"), tourData.min));
+
+		table.GetRow(3, tourData);
+		m_textMax14->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin14->SetValue(wxString::Format(_("%d"), tourData.min));
+
+		table.GetRow(4, tourData);
+		m_textMax18->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin18->SetValue(wxString::Format(_("%d"), tourData.min));
+
+		table.GetRow(5, tourData);
+		m_textMax116->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin116->SetValue(wxString::Format(_("%d"), tourData.min));
+
+		table.GetRow(6, tourData);
+		m_textMax132->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin132->SetValue(wxString::Format(_("%d"), tourData.min));
+
+		table.GetRow(7, tourData);
+		m_textMax164->SetValue(wxString::Format(_("%d"), tourData.max));
+		m_textMin164->SetValue(wxString::Format(_("%d"), tourData.min));
+
+	}while(0);
 }
 
--- gui/src/udfStartNumberAssignDlg.cpp
+++ gui/src/udfStartNumberAssignDlg.cpp
@@ -9,11 +9,11 @@ udfStartNumberAssignDlg::udfStartNumberAssignDlg( wxWindow* parent, unsigned int
 , m_nCsId(nCsId)
 {
 	m_pCon = CDbManager::Instance()->GetConnection();
-			
+
 	RefreshList();
-	
+
 	srand(time(NULL));
-	
+
 	m_textSearch->SetFocus();
 }
 
@@ -21,18 +21,18 @@ void udfStartNumberAssignDlg::RefreshList()
 {
 	CChampionshipTeamsTable::tDATA 	filter = {0};
 	filter.championshipId = m_nCsId;
-	
+
 	CChampionshipTeamsTable(m_pCon).Find(m_CsTeams, filter);
-	
+
 	m_listTeams->Clear();
-	
+
 	CChampionshipTeamsTable::tTableIt it = m_CsTeams.begin();
 	while(it != m_CsTeams.end())
 	{
 		CChampionshipTeamsTable::tDATA& data = it->second;
 		int nPos = m_listTeams->GetCount();
 		m_listTeams->Insert(data.name, nPos, (void*)&it->first);
-		
+
 		it++;
 	}
 }
@@ -41,19 +41,19 @@ void udfStartNumberAssignDlg::OnSearch( wxCommandEvent& event )
 {
 	wxString search = m_textSearch->GetValue().Upper();
 	CChampionshipTeamsTable::tTableIt item;
-	
+
 	m_listTeams->Clear();
 	for(item = m_CsTeams.begin(); item != m_CsTeams.end(); item++)
 	{
 		CChampionshipTeamsTable::tDATA& data = item->second;
-		
+
 		if(wxString(data.name).Upper().Contains(search))
 		{
 			int pos = m_listTeams->GetCount();
 			m_listTeams->Insert(data.name, pos, (void*)&item->first);
 		}
 	}
-	
+
 	if(0 < m_listTeams->GetCount())
 	{
 		m_listTeams->SetSelection(0);
@@ -67,60 +67,22 @@ void udfStartNumberAssignDlg::OnSelectTeam( wxCommandEvent& event )
 {
 
 }
-/*
-void udfStartNumberAssignDlg::OnRandomFind( wxCommandEvent& event )
-{
-	unsigned long startNum = m_nLastAssign;
-	CChampionshipTeamsTable::tTableMap data;
-	
-	do
-	{
-		startNum = rand()%9999;
-		
-		m_Filter.startNumber = startNum;
-		
-		m_pTable->Find(data, m_Filter);
-		
-		if(data.size() == 0)
-			break;
-	}while(1);
-	
-	m_textNumber->SetValue(wxString::Format(STR_FORMAT_START_NUMBER, startNum));
-}
 
-unsigned long udfStartNumberAssignDlg::FindNextNum(unsigned long num)
-{
-	unsigned long startNum = num;
-	CChampionshipTeamsTable::tTableMap data;
-	
-	do
-	{
-		m_Filter.startNumber = ++startNum;
-		
-		m_pTable->Find(data, m_Filter);
-		
-		if(data.size() == 0)
-			break;
-	}while(1);
-	
-	return startNum;
-}
-*/
 void udfStartNumberAssignDlg::OnAssign( wxCommandEvent& event )
 {
 	do{
 		unsigned long startNum = 1;
 	/*	if(!m_textNumber->GetValue().ToULong(&startNum))
 			break;
-			
+
 		int nPos = m_listTeams->GetSelection();
-			
+
 		if(-1 == nPos)
 			break;
 
 		int nId = *(int*)m_listTeams->GetClientData(nPos);
 		CChampionshipTeamsTable::tDATA data = {0};
-		
+
 		if (0 == data.startNumber)
 		{
 			data.startNumber = startNum;
@@ -131,7 +93,7 @@ void udfStartNumberAssignDlg::OnAssign( wxCommandEvent& event )
 		{
 			m_textNumber->SetValue(wxString::Format(STR_FORMAT_START_NUMBER, (unsigned long)data.startNumber));
 		}
-		
+
 		m_textSearch->SetValue("");
 		m_textSearch->SetFocus();
 		 * */
@@ -145,11 +107,11 @@ void udfStartNumberAssignDlg::OnSave( wxCommandEvent& event )
 	{
 		CChampionshipTeamsTable::tDATA	newData = {0};
 		CChampionshipTeamsTable::tDATA& data = it->second;
-		
+
 		newData.startNumber = data.startNumber;
-		
+
 		CChampionshipTeamsTable(m_pCon).UpdateRow(data.id, newData);
-		
+
 		it++;
 	}
 	EndModal(wxID_OK);
@@ -168,9 +130,9 @@ void udfStartNumberAssignDlg::OnAllAssignIncrease(wxCommandEvent& event)
 	{
 		CChampionshipTeamsTable::tDATA& data = it->second;
 		data.startNumber = nStartNum;
-		
+
 		m_StNumMap[nStartNum] = data.id;
-		
+
 		nStartNum++;
 		it++;
 	}
@@ -183,14 +145,14 @@ void udfStartNumberAssignDlg::OnAllAssignRandom(wxCommandEvent& event)
 	while(it != m_CsTeams.end())
 	{
 		CChampionshipTeamsTable::tDATA& data = it->second;
-		
+
 		do
 		{
 			nStartNum = rand()%9999;
 		}while(m_StNumMap.end() != m_StNumMap.find(nStartNum));
-		
+
 		data.startNumber = nStartNum;
-		
+
 		it++;
 	}
 }
@@ -198,7 +160,7 @@ void udfStartNumberAssignDlg::OnAllAssignRandom(wxCommandEvent& event)
 void udfStartNumberAssignDlg::OnFindNext(wxCommandEvent& event)
 {
 	unsigned int nStartNum = m_StNumMap.rbegin()->second;
-		
+
 	m_textNumber->SetValue(wxString::Format(STR_FORMAT_START_NUMBER, nStartNum));
 }
 
@@ -209,7 +171,7 @@ void udfStartNumberAssignDlg::OnFindRandom(wxCommandEvent& event)
 	{
 		nStartNum = rand()%9999;
 	}while(m_StNumMap.end() != m_StNumMap.find(nStartNum));
-	
+
 	m_textNumber->SetValue(wxString::Format(STR_FORMAT_START_NUMBER, nStartNum));
 }
 
