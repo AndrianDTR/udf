5c6a993fd590a79588032d0c05dacaaa81756c23
Andrian <andrian.yablonsky@gmail.com>
2012-11-29 01:35:01 +0200

    Add Gui resource examples

---
+++ gui/src/resource.h
@@ -0,0 +1,37 @@
+#ifndef __resource_h__
+#define __resource_h__
+
+#define	IDI_MSG_WARNING					101
+#define	IDI_MSG_CONFIRMATION			102
+#define	IDI_MSG_LOGIN					103
+
+#define	IDI_ICON_ANTENNA_INFO			301
+#define	IDI_ICON_ANTENNA_SELECT			302
+#define	IDI_ICON_FUNC_TEST				303
+#define	IDI_ICON_DTC_OVERVIEW			304
+#define	IDI_ICON_DTC_LIST				305
+#define	IDI_ICON_EPG_DATA				306
+#define	IDI_ICON_GPS_DATA				307
+#define	IDI_ICON_GPS_INIT				308
+#define	IDI_ICON_EWS_DATA				309
+#define	IDI_ICON_PROGRAMDATA			310
+#define	IDI_ICON_SOURCE					311
+#define	IDI_ICON_STATIONLIST			312
+#define	IDI_ICON_TELETEXT				313
+#define	IDI_ICON_TEMPANDFAN				314
+#define	IDI_ICON_MODULECTRL				315
+#define	IDI_ICON_TERMINALMODE			316
+#define	IDI_ICON_TERMINAL				317
+#define	IDI_ICON_SEARCHMODULES			318
+#define	IDI_ICON_TVMODULEDATA			319
+#define	IDI_ICON_FLASH					320
+#define	IDI_ICON_COMMENT				321
+#define	IDI_ICON_TESTDRIVE				322
+#define	IDI_ICON_POWERSUPPLY			323
+#define	IDI_ICON_DIVERSITY				324
+#define	IDI_ICON_DOWNLOAD				325
+
+#define	IDI_ICON_LOGIN					401
+#define	IDI_ICON_SAVEALLDATA			402
+
+#endif // __resource_h__
